09bd3c73b96727559e1d7a035d168b4963c9268e
Seyma000 <serterseyma254@gmail.com>
2024-05-19 21:46:45 +0300

    Initial commit

---
+++ src/main/java/dev/seyma/VeterinaryProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.seyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryProjectApplication.class, args);
+	}
+
+}
